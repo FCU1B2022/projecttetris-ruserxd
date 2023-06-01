23a2008fc31bf7df9357f8392bb73d6a9335e50f
ruserxd <zxcmomo0219@gmail.com>
2023-06-01 12:08:03 +0800

    06/01

--- D1149580_project_for_tetris/main.c
+++ D1149580_project_for_tetris/main.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
 #include <windows.h>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning (disable:4996)
 
 #define LEFT_KEY 0x25     // The key to move left, default = 0x25 (left arrow)
 #define RIGHT_KEY 0x27    // The key to move right, default = 0x27 (right arrow)
 #define ROTATE_KEY 0x26   // The key to rotate, default = 0x26 (up arrow)
 #define DOWN_KEY 0x28     // The key to move down, default = 0x28 (down arrow)
 #define FALL_KEY 0x20     // The key to fall, default = 0x20 (spacebar)
-
+#define LEAVE_KEY 0x1B    // The key to leave
 #define FALL_DELAY 500    // The delay between each fall, default = 500
 #define RENDER_DELAY 100  // The delay between each frame, default = 100
 
@@ -18,7 +20,7 @@
 #define ROTATE_FUNC() GetAsyncKeyState(ROTATE_KEY) & 0x8000
 #define DOWN_FUNC() GetAsyncKeyState(DOWN_KEY) & 0x8000
 #define FALL_FUNC() GetAsyncKeyState(FALL_KEY) & 0x8000
-
+#define Leave_FUNC() GetAsyncKeyState(LEAVE_KEY)&0x8000
 #define CANVAS_WIDTH 10
 #define CANVAS_HEIGHT 20
 
@@ -391,7 +393,7 @@ int clearLine(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH]) {
     return linesCleared;
 }
 
-void logic(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state)
+int logic(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state)
 {
     if (ROTATE_FUNC()) {
         int newRotate = (state->rotate + 1) % 4;
@@ -418,7 +420,11 @@ void logic(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state)
     else if (FALL_FUNC()) {
         state->fallTime += FALL_DELAY * CANVAS_HEIGHT;
     }
-
+    else if (Leave_FUNC()) {
+        system("cls");
+        Sleep(100);
+        return -1;
+    }
     state->fallTime += RENDER_DELAY;
 
     while (state->fallTime >= FALL_DELAY) {
@@ -446,44 +452,146 @@ void logic(Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH], State* state)
             }
         }
     }
-    return;
+    return 0;
+}
+//
+//選擇要玩的遊戲
+int drawArcadeMenu()
+{
+    printf("========== 街機遊戲選單 ==========\n");
+    printf("1. ooxx\n");
+    printf("2. 俄羅斯方塊\n");
+    printf("0. 可直接退出遊戲\n");
+    printf("==================================\n");
+    printf("輸入想玩啥?\n");
+    int n = 1;
+    scanf("%d", &n); 
+    if (n == 0)exit(0);
+    Sleep(1000);
+    return n;
+}
+//
+//
+//OOXX的遊戲
+bool jg_win(char array[9]) {
+    if (array[0] == 'o' && array[1] == 'o' && array[2] == 'o')return true;
+    else if (array[3] == 'o' && array[4] == 'o' && array[5] == 'o')return true;
+    else if (array[6] == 'o' && array[7] == 'o' && array[8] == 'o')return true;
+    else if (array[0] == 'o' && array[4] == 'o' && array[8] == 'o')return true;
+    else if (array[2] == 'o' && array[4] == 'o' && array[6] == 'o')return true;
+    else if (array[0] == 'o' && array[3] == 'o' && array[6] == 'o')return true;
+    else if (array[1] == 'o' && array[4] == 'o' && array[7] == 'o')return true;
+    else if (array[2] == 'o' && array[5] == 'o' && array[8] == 'o')return true;
+    if (array[0] == 'x' && array[1] == 'x' && array[2] == 'x')return true;
+    else if (array[3] == 'x' && array[4] == 'x' && array[5] == 'x')return true;
+    else if (array[6] == 'x' && array[7] == 'x' && array[8] == 'x')return true;
+    else if (array[0] == 'x' && array[4] == 'x' && array[8] == 'x')return true;
+    else if (array[2] == 'x' && array[4] == 'x' && array[6] == 'x')return true;
+    else if (array[0] == 'x' && array[3] == 'x' && array[6] == 'x')return true;
+    else if (array[1] == 'x' && array[4] == 'x' && array[7] == 'x')return true;
+    else if (array[2] == 'x' && array[5] == 'x' && array[8] == 'x')return true;
+    return false;
+}
+void print_gaming(char array[9], int meo[9]) {
+    system("cls");
+    for (int i = 0; i < 9; i++) {
+        printf("\033[43;30m");
+        if (meo[i] != 0)
+            printf("\033[31;1m%c \033[0m", array[i]);
+        else
+            printf("%d \033[0m", i);
+        if (i % 3 == 2)printf("\n");
+        printf("\033[0m");
+    }
+}
+int edge_wrong(int x) {
+    if (x < 0 || x>8) {
+        printf("You exceed the edge please try again!!!\n");
+        return 0;
+    }
+    return 1;
 }
 
-int main()
-{
-    srand(time(NULL));
-    State state = {
-        .x = CANVAS_WIDTH / 2,
-        .y = 0,
-        .score = 0,
-        .rotate = 0,
-        .fallTime = 0
-    };
-
-    for (int i = 0; i < 4; i++)
-    {
-        state.queue[i] = rand() % 7;
-    }
-
-    Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH];
-    for (int i = 0; i < CANVAS_HEIGHT; i++)
-    {
-        for (int j = 0; j < CANVAS_WIDTH; j++)
-        {
-            resetBlock(&canvas[i][j]);
+int OOXX() {
+    char array[9] = { 0 };
+    int meo[9] = { 0 };
+    printf("Hello I will like to start a game name for OOXX\n");
+    int want_to_play = 1;//0==o 1==x
+    while (want_to_play) {
+        int n = 0;
+        memset(array, '0', sizeof(array));
+        memset(meo, 0, sizeof(meo));
+        print_gaming(array, meo);
+        while (!jg_win(array)) {
+            int x;
+            scanf("\n%d", &x);
+            if (edge_wrong(x)){
+                if (n % 2 == 0)array[x] = 'o';
+                else if (n % 2 == 1)array[x] = 'x';
+                meo[x] = 1;
+                n++;
+            }
+            print_gaming(array, meo);
         }
+        if (n == 9 && !jg_win(array))printf("平手\n");
+        else if (n % 2 == 1)printf("O winner\n");
+        else if (n % 1 == 0) printf("x winner\n");
+        printf("Do you wnat to play again??(Yes==1,No==0)\n");
+        scanf("%d", &want_to_play);
     }
+    return 0;
+}
+int main()
+{  
+    int want_to_play = 1;
+    while (want_to_play) {
+        system("cls");
+        int n = drawArcadeMenu();
+        printf("%d\n", n);
+        srand((unsigned)time(NULL));
+        if (n == 1) {
+            system("cls");
+            OOXX();
+        }
+        else if (n == 2) {
+            State state = {
+                .x = CANVAS_WIDTH / 2,
+                .y = 0,
+                .score = 0,
+                .rotate = 0,
+                .fallTime = 0
+            };
 
-    system("cls");
-    // printf("\e[?25l"); // hide cursor
+            for (int i = 0; i < 4; i++)
+            {
+                state.queue[i] = rand() % 7;
+            }
 
-    move(canvas, state.x, state.y, state.rotate, state.x, state.y, state.rotate, state.queue[0]);
+            Block canvas[CANVAS_HEIGHT][CANVAS_WIDTH];
+            for (int i = 0; i < CANVAS_HEIGHT; i++)
+            {
+                for (int j = 0; j < CANVAS_WIDTH; j++)
+                {
+                    resetBlock(&canvas[i][j]);
+                }
+            }
 
-    while (1)
-    {
-        logic(canvas, &state);
-        printCanvas(canvas, &state);
-        Sleep(100);
+            system("cls");
+
+            move(canvas, state.x, state.y, state.rotate, state.x, state.y, state.rotate, state.queue[0]);
+
+            while (1)
+            {
+                logic(canvas, &state);
+                printCanvas(canvas, &state);
+                printf("\n\n\n\n\n\n\n\n\n\n\n\Your score:%d", state.score);
+                Sleep(100);
+                if (logic(canvas, &state) == -1) {
+                    break;
+                }
+            }
+        }
+        printf("還要再玩其他遊戲或繼續玩一樣的嗎?");
+        scanf("\n%d",&want_to_play);
     }
-
 }
